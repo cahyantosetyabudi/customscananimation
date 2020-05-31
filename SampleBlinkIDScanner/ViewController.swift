5a1121cd2e2312810f17d8b051826e11f120d57b
Cahyanto Setya Budi <cahyantosetyabudi@gmail.com>
2020-05-31 14:48:19 +0700

    Initial Commit

---
+++ SampleBlinkIDScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleBlinkIDScanner
+//
+//  Created by Cahyanto Setya Budi on 31/05/20.
+//  Copyright © 2020 Cahyanto Setya Budi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
